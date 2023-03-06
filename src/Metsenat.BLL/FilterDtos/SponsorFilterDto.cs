e713aae3599d2ba4f7ab1724da182a36d15916e4
abuUsman_04 <i123l183l@gmail.com>
2023-03-06 11:44:07 +0300

    some changes

--- src/Metsenat.BLL/FilterDtos/SponsorFilterDto.cs
+++ src/Metsenat.BLL/FilterDtos/SponsorFilterDto.cs
@@ -4,6 +4,7 @@ namespace Metsenat.BLL.FilterDtos;
 
 public class SponsorFilterDto
 {
+    //comment
     public decimal? Amount { get; set; }
     public ESponsorFilter? SponsorFilter { get; set; }
     public DateTime? CreatedDate { get; set; }
